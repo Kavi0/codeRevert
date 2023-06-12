7b14e4b13f3fd85f966748abf262e668b35f2454
Kavibharathi P <kavibharathi.pari@gmail.com>
2023-06-12 17:33:51 +0530

    Wrong commit wantedly

--- src/test/java/com/pack/codeRevert/CodeRevert.java
+++ src/test/java/com/pack/codeRevert/CodeRevert.java
@@ -14,7 +14,7 @@ public class CodeRevert {
 
 	public void thirdUpdateWrongCode() {
 
-		System.out.println("thirdUpdateWrongCode");
+		System.out.println("statement changed");
 	}
 
 	public static void main(String[] args) {
